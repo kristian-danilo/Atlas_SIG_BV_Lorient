39c26870baefe2c2a8ac41f1d1cc5a8711451262
kristian-danilo <147108717+kristian-danilo@users.noreply.github.com>
2026-05-12 16:00:19 +0200

    Add files via upload

--- styles/CarrresetsitesdintretgologiquedupaysdeLorient_14_style.js
+++ styles/CarrresetsitesdintretgologiquedupaysdeLorient_14_style.js
@@ -1,5 +1,99 @@
 var size = 0;
 var placement = 'point';
+function categories_CarrresetsitesdintretgologiquedupaysdeLorient_14(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '1':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(255,0,64,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.2 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.6599999999999997}), fill: new ol.style.Fill({color: 'rgba(255,0,64,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(96,176,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.2 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.6599999999999997}), fill: new ol.style.Fill({color: 'rgba(96,176,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '5':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(0,72,96,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '6':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.2 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.6599999999999997}), fill: new ol.style.Fill({color: 'rgba(0,72,96,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '7':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(192,240,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '8':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.2 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.6599999999999997}), fill: new ol.style.Fill({color: 'rgba(192,240,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '9':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(0,128,32,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '10':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.2 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.6599999999999997}), fill: new ol.style.Fill({color: 'rgba(0,128,32,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
 
 var style_CarrresetsitesdintretgologiquedupaysdeLorient_14 = function(feature, resolution){
     var context = {
@@ -8,55 +102,22 @@ var style_CarrresetsitesdintretgologiquedupaysdeLorient_14 = function(feature, r
     };
     
     var labelText = ""; 
-    var value = feature.get("var value = '';");
-    var labelFont = "13.0px \'Arial\', sans-serif";
+    var value = feature.get("Etiquette_Qgis2Web");
+    var labelFont = "7.800000000000001px \'Arial\', sans-serif";
     var labelFill = "#323232";
-    var bufferColor = "";
-    var bufferWidth = 0;
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.5;
     var textAlign = "left";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
-    if ("" !== null) {
-        labelText = String("");
+    if (feature.get("fid") !== null) {
+        labelText = String(feature.get("fid"));
     }
     
-        function rules_CarrresetsitesdintretgologiquedupaysdeLorient_14(feature, value) {
-            var context = {
-                feature: feature,
-                variables: {}
-            };
-            // Start of if blocks and style check logic
-            if (exp_CarrresetsitesdintretgologiquedupaysdeLorient_14rule0_eval_expression(context)) {
-                      return [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(224,112,0,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    }
-                    else if (exp_CarrresetsitesdintretgologiquedupaysdeLorient_14rule1_eval_expression(context)) {
-                      return [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,16,64,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}), fill: new ol.style.Fill({color: 'rgba(128,0,255,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    }
-            else {
-                return [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(192,192,0,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-            }
-        }
-        var style = rules_CarrresetsitesdintretgologiquedupaysdeLorient_14(feature, value);
-        ;
+    var style = categories_CarrresetsitesdintretgologiquedupaysdeLorient_14(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
 
     return style;
 };
